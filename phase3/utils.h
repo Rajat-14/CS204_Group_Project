3a34c219a63f13607dc89e3369aa88b1992bf0b2
rohit4kk <rohitverma98999@gmail.com>
2025-04-21 19:27:37 +0530

    Branch Prediction

--- phase3/utils.h
+++ phase3/utils.h
@@ -9,4 +9,5 @@ long long convert_hexstr2int(const string &hexstr, int numBits);
 unsigned int convert_binstr2int(const string &binstr);
 string convert_PC2hex(unsigned int num);
 
+
 #endif // UTILS_H
