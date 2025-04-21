9233d5a60b54ec8b7b2fc0a6dffce721497c0aa9
rohit4kk <rohitverma98999@gmail.com>
2025-04-21 19:26:22 +0530

    Branch Prediction

--- phase3/simulator.cpp
+++ phase3/simulator.cpp
@@ -6,12 +6,14 @@ map<string, string> instruction_map;
 map<string, string> data_map;
 string instructionRegister;
 string currentPC_str = "0x0";
+bool first_ever_fetch = true;
+int branch_control = -1;
+bool jumped_to_terminate = false;
 unsigned int returnAddress = 0;
 long long int RM;
 long long int aluResult;
 long long int RY;
 long long int memoryData;
-bool is_PCupdated_while_execution = false;
 int R[32] = {0};
 long long int clockCycle = 0;
 int numStallNeeded = 0;
@@ -19,38 +21,54 @@ bool pipelineEnd = false;
 instruction_register ir;
 bool stallingWhileDataForwarding = false;
 bool isFlushingDone = false;
-map<string, pair<int, string>> branchPredictor;
-bool controlForIAG = false;
-bool exitLoop = false;
-bool stopFetch = false;
-bool start = true;
-bool stalled = false;
-bool stalledM = false;
+bool flush1 = false;
+bool flush2 = false;
+bool fetching_in_flushing = false;
+
+
+bool        PHT[BTB_SIZE]         = {false};
+unsigned int BTB_target[BTB_SIZE] = {0};
+bool        BTB_valid[BTB_SIZE]   = {false};
+bool        prediction_used[BTB_SIZE] = {false};
+
+
 
 //---------------- Pipeline Registers (Phase 3) ----------------
+
 IF_ID if_id = {"", "", false};
 ID_EX id_ex = {0, 0, 0, 0, 0, 0, 0, 0, 0, "", "", 0, false, false, "", false, false, 0, false, 0};
-EX_MEM ex_mem = {0, 0, false, false, false, 0, false, 0, "", false, false};
-MEM_WB mem_wb = {0, 0, false, false};
+EX_MEM ex_mem = {0, 0, false, false, false, 0, false, 0,""};
+MEM_WB mem_wb = {0, 0, false, false,""};
 
 //---------------- Knobs (Default Settings) ----------------
 bool enablePipelining = true;
-bool enableDataForwarding = false;
-bool printRegisterFile = false;
+bool enableDataForwarding = true;
+bool printRegisterFile = true;
 bool printPipelineTrace = false;
-int traceInstructionNumber = 0; // 0 means disabled
-bool printBranchPredictor = false;
+bool printSpecificInstruction=true; //knob 5
+int traceInstructionNumber = 9; 
+bool printBranchPredictor = true;
 
 //---------------- Statistics Counters ----------------
 unsigned long totalCycles = 0;
 unsigned long totalInstructions = 0;
-unsigned long aluInstructionCount = 0;
-unsigned long dataTransferCount = 0;
-unsigned long controlInstructionCount = 0;
+set<string> aluInstructionCount ;
+set<string> dataTransferCount;
+set<string> controlInstructionCount;
 unsigned long stallCount = 0;
 unsigned long dataHazardCount = 0;
 unsigned long controlHazardCount = 0;
 unsigned long branchMispredictionCount = 0;
+unsigned long stallsDueToControlHazard=0;
+
+
+  bool fetchFlag=false;
+  bool decodeFlag=false;
+  bool executeFlag=false;
+  bool memoryFlag=false;
+  bool writebackFlag=false;
+
+
 
 void load_mc_file(const string &filename)
 {
@@ -117,4 +135,92 @@ void load_mc_file(const string &filename)
     }
 
     file.close();
+}
+
+void print()
+{
+    cout << "Clock: " << clockCycle << endl;
+    if (printRegisterFile)
+    {
+        for (auto it : R)
+        {
+            cout << it << " ";
+        }
+        cout << endl;
+    }
+    cout << "----------------------------------------------------------------------------------------------------\n";
+    logPipelineRegisters();
+    printBTB();
+}
+
+void logPipelineRegisters()
+{
+    // if(printSpecificTrace){
+    //   checkNthInstruction(traceInstructionNumber);
+    // }
+
+    if (printPipelineTrace)
+    {
+        std::ofstream ofs("pipeline_registers.txt", std::ios::app);
+        if (!ofs.is_open())
+            return;
+
+        ofs << "Clock Cycle: " << clockCycle << "\n";
+
+        // IF/ID stage
+        ofs << "IF/ID Stage:\n";
+        ofs << "  instruction: " << if_id.instruction << "\n";
+        ofs << "  pc:          " << if_id.pc << "\n";
+        ofs << "  isValid:     " << if_id.isValid << "\n\n";
+
+        // ID/EX stage
+        ofs << "ID/EX Stage:\n";
+        ofs << "  opcode:      " << id_ex.opcode << "\n";
+        ofs << "  rd:          " << id_ex.rd << "\n";
+        ofs << "  rs1:         " << id_ex.rs1 << "\n";
+        ofs << "  rs2:         " << id_ex.rs2 << "\n";
+        ofs << "  funct3:      " << id_ex.funct3 << "\n";
+        ofs << "  funct7:      " << id_ex.funct7 << "\n";
+        ofs << "  imm:         " << id_ex.imm << "\n";
+        ofs << "  valid:       " << id_ex.valid << "\n\n";
+
+        // EX/MEM stage
+        ofs << "EX/MEM Stage:\n";
+        ofs << "  aluResult:   " << ex_mem.aluResult << "\n";
+        ofs << "  rd:          " << ex_mem.rd << "\n";
+        ofs << "  valid:       " << ex_mem.valid << "\n\n";
+
+        // MEM/WB stage
+        ofs << "MEM/WB Stage:\n";
+        ofs << "  writeData:   " << mem_wb.writeData << "\n";
+        ofs << "  rd:          " << mem_wb.rd << "\n";
+        ofs << "  valid:       " << mem_wb.valid << "\n";
+
+        ofs << "-------------------------------------------\n\n";
+    }
+}
+
+
+
+
+void printBTB()
+{
+    if (printBranchPredictor)
+    {
+        for (int i = 0; i < BTB_SIZE; i++)
+        {
+            if (BTB_target[i] != 0)
+            {
+                cout << "PC is: " << convert_PC2hex(i * 4) << " Target Address is: " << BTB_target[i];
+                if (PHT[i] == 1)
+                {
+                    cout << " The State of predictor is: TAKEN\n";
+                }
+                else
+                {
+                    cout << " The State of predictor is: NOT TAKEN\n";
+                }
+            }
+        }
+    }
 }
